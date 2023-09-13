c8fbe7f3da62dc5ee5a13bcb159ba59a70ccf752
Joao Gabriel <joaogabrielferr@gmail.com>
2023-09-12 22:07:16 -0300

    feat: adding editor.js and adjusting main layout

--- src/app/features/note/pages/note/note.component.ts
+++ src/app/features/note/pages/note/note.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-note',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FontAwesomeModule,FormsModule],
   templateUrl: './note.component.html',
   styleUrls: ['./note.component.scss']
 })
 export class NoteComponent implements OnInit{
 
+  faBars = faBars;
+
+  editor! : EditorJS;
+
+  noteTitle : string = "";
+
   constructor(private route : ActivatedRoute){}
+
+
 
   note : any;
   id : any;
@@ -20,7 +34,45 @@ export class NoteComponent implements OnInit{
   {
     this.route.paramMap.subscribe((params)=>{
       this.id = params.get('id');
-    })
+    });
+
+    this.initializeEditor();
+
+    this.saveData();
+
+
   }
+
+  initializeEditor()
+  {
+    this.noteTitle = "Untitled";
+    this.editor  = new EditorJS({
+      holder: 'editorjs',
+      placeholder: "Type here to write your post...",
+      tools:{
+        header:Header
+      },
+    });
+
+    this.editor.isReady.then(()=>{
+      console.log("editor is ready");
+    },(error)=>{
+      console.log("editor couldnt initialized because of " + error);
+    })
+
+  }
+
+  saveData()
+  {
+    setInterval(()=>{
+      this.editor.save().then((outputData) => {
+        console.log('Article data: ', outputData)
+      }).catch((error) => {
+        console.log('Saving failed: ', error)
+      });
+    },10000);
+  }
+
+
 
 }
